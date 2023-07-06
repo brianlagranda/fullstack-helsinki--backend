4e65b5195dcb18839f86284170a5a8d530c0bd15
bra20019613 <brianlagranda@gmail.com>
2023-07-06 17:43:44 -0300

    Exercises 3.13.-3.14. - Done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
+
+const Person = require("./models/person");
 
 const app = express();
 
-// app.use(morgan("tiny")); -- Code for exercise 3.7, i've used custom config on 3.8
-// so i don't need tiny config anymore.
+app.use(cors());
 
 app.use(express.static("build"));
 
@@ -20,35 +24,14 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>You should enter /api/persons after port in the url</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -60,14 +43,9 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -77,47 +55,29 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const generateRandomId = () => {
-  const maxId = 9999999;
-  const randomId = Math.floor(Math.random() * maxId);
-
-  return randomId;
-};
-
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.name) {
+  if (body.name === undefined) {
     return response.status(400).json({
       error: "name missing",
     });
   }
 
-  if (!body.number) {
+  if (body.number === undefined) {
     return response.status(400).json({
       error: "number missing",
     });
   }
 
-  if (
-    persons.some(
-      (p) => p.name.toLocaleLowerCase() === body.name.toLocaleLowerCase()
-    )
-  ) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
-    id: generateRandomId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -126,7 +86,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
