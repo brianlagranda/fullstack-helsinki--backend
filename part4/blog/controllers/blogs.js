fc432cbdc9f20449c37796abb8a4a06ea18eec07
bra20019613 <brianlagranda@gmail.com>
2023-07-13 15:51:40 -0300

    Exercise 4.4. - Done

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -7,6 +7,18 @@ blogsRouter.get("/", (request, response) => {
   });
 });
 
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
 blogsRouter.post("/", (request, response, next) => {
   const blog = new Blog(request.body);
 
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,19 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1;
+};
+
+const emptyList = () => {
+  return "";
+};
+
+const totalLikes = (blogs) => {
+  let sum = 0;
+  blogs.forEach((blog) => (sum = sum + blog.likes));
+  return sum;
 };
 
 module.exports = {
   dummy,
+  emptyList,
+  totalLikes,
 };
