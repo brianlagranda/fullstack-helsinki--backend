fac708f2979986c506497f5173e2d55e11bec50e
bra20019613 <brianlagranda@gmail.com>
2023-07-14 14:03:55 -0300

    Exercise 4.5. - Done

--- part4/blog/.eslintrc.js
+++ part4/blog/.eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     ecmaVersion: "latest",
   },
   rules: {
-    indent: ["error", 2],
+    indent: "off",
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -12,8 +12,26 @@ const totalLikes = (blogs) => {
   return sum;
 };
 
+const favoriteBlog = (blogs) => {
+  const mostLiked = blogs.reduce((mostLiked, currentBlog) =>
+    mostLiked.likes > currentBlog.likes
+      ? {
+          title: mostLiked.title,
+          author: mostLiked.author,
+          likes: mostLiked.likes,
+        }
+      : {
+          title: currentBlog.title,
+          author: currentBlog.author,
+          likes: currentBlog.likes,
+        }
+  );
+  return mostLiked;
+};
+
 module.exports = {
   dummy,
   emptyList,
   totalLikes,
+  favoriteBlog,
 };
