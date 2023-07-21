91f7a7eff21429b01b20f49466d7b44a7123f9a9
bra20019613 <brianlagranda@gmail.com>
2023-07-21 17:09:01 -0300

    Exercise 4.10.-4.12 -

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,85 +1,99 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 
 const Blog = require("../models/blog");
 
-const initialBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-];
-
 beforeEach(async () => {
   await Blog.deleteMany({});
-  let blogObject = new Blog(initialBlogs[0]);
+
+  let blogObject = new Blog(helper.initialBlogs[0]);
   await blogObject.save();
-  blogObject = new Blog(initialBlogs[1]);
+
+  blogObject = new Blog(helper.initialBlogs[1]);
   await blogObject.save();
 });
 
-describe("correct amount of blog posts", () => {
-  test("all blog posts are returned", async () => {
-    const response = await api.get("/api/blogs");
+test("all blog posts are returned", async () => {
+  const response = await api.get("/api/blogs");
 
-    expect(response.body).toHaveLength(initialBlogs.length);
-  });
-
-  test("a specific blog post is within the returned blogs", async () => {
-    const response = await api.get("/api/blogs");
-
-    const title = response.body.map((r) => r.title);
-    expect(title).toContain("Go To Statement Considered Harmful");
-  });
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
-describe("unique identifier property of the blog posts is named id", () => {
-  test("every blog posts id is defined", async () => {
-    const response = await api.get("/api/blogs");
+test("a specific blog post is within the returned blogs", async () => {
+  const response = await api.get("/api/blogs");
 
-    const blogId = response.body.map((r) => r.id);
-    expect(blogId).toBeDefined();
-  });
+  const title = response.body.map((r) => r.title);
+  expect(title).toContain("Go To Statement Considered Harmful");
 });
 
-describe("creating new blog post", () => {
-  test("a valid blog post can be added", async () => {
-    const newBlogPost = {
-      _id: "5a422bc61b54a676234d17fe",
-      title: "From my life",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/users/EWD/ewd11xx/EWD1166.PDF",
-      likes: 0,
-      __v: 0,
-    };
+test("every blog posts unique identifier is named id", async () => {
+  const response = await api.get("/api/blogs");
 
-    await api
-      .post("/api/blogs")
-      .send(newBlogPost)
-      .expect(201)
-      .expect("Content-Type", /application\/json/);
+  const blogId = response.body.map((r) => r.id);
+  expect(blogId).toBeDefined();
+});
 
-    const response = await api.get("/api/blogs");
+test("a valid blog post can be added", async () => {
+  const newBlogPost = {
+    _id: "5a422bc61b54a676234d17fe",
+    title: "From my life",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/users/EWD/ewd11xx/EWD1166.PDF",
+    likes: 0,
+    __v: 0,
+  };
 
-    const title = response.body.map((r) => r.title);
+  await api
+    .post("/api/blogs")
+    .send(newBlogPost)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
 
-    expect(response.body).toHaveLength(initialBlogs.length + 1);
-    expect(title).toContain("From my life");
-  });
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  const title = blogsAtEnd.map((r) => r.title);
+  expect(title).toContain("From my life");
+});
+
+test("blog posts added with no likes by default will have zero likes", async () => {
+  const newBlogPost = {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    __v: 0,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlogPost)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  console.log(blogsAtEnd[-1]["likes"]);
+
+  expect(blogsAtEnd[-1]["likes"]).toBe(0);
+});
+
+test("if the title or url are missing", async () => {
+  const newBlogPost = {
+    _id: "5a422bc61b54a676234d17fc",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    __v: 0,
+  };
+
+  await api.post("/api/blogs").send(newBlogPost).expect(400);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
 });
 
 afterAll(async () => {
