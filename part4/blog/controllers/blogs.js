b7dfe5b87d0443570461577182af5826c5919729
bra20019613 <brianlagranda@gmail.com>
2023-08-02 17:55:02 -0300

    Exercises 4.17.-4.19 - Fixed

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -5,13 +5,21 @@ const User = require("../models/user");
 require("express-async-errors");
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
 
   const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token invalid" });
   }
   const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  });
 
   const savedBlog = await blog.save();
   user.blogs = user.blogs.concat(savedBlog._id);
@@ -22,13 +30,11 @@ blogsRouter.post("/", async (request, response) => {
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
-    id: 1,
     username: 1,
     name: 1,
-    blogs: 1,
   });
 
-  response.json(blogs);
+  response.json(blogs).status(200);
 });
 
 blogsRouter.get("/:id", async (request, response) => {
--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -29,7 +29,11 @@ usersRouter.post("/", async (request, response, next) => {
 });
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({}).populate("blogs");
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    url: 1,
+    author: 1,
+  });
 
   response.json(users);
 });
